bb1f13c02f52b7c4bab1f965412aa3507402054b
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2020-05-15 12:45:48 -0400

    Update sumDigits.java

--- sumDigits.java
+++ sumDigits.java
@@ -6,3 +6,17 @@
 //sumDigits(126) → 9
 //sumDigits(49) → 13
 //sumDigits(12) → 3
+
+public int sumDigits(int n) {
+
+  if(n == 1){
+    return n;
+  }
+  if(n == 0){
+    return 0;
+  }
+  if ( n%10 != 0){
+    return n%10 + sumDigits(n/10);
+  }
+  return sumDigits(n/10);
+}
