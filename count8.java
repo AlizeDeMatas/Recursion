cf49f3251d84c5e0b908c20654218eddb9f68041
AlizeDeMatas <32222634+AlizeDeMatas@users.noreply.github.com>
2020-05-18 22:37:47 -0400

    Update count8.java

--- count8.java
+++ count8.java
@@ -7,3 +7,9 @@
 //count8(8) → 1
 //count8(818) → 2
 //count8(8818) → 4
+public int count8(int n) {
+  if (n < 1) return 0;
+  if (n % 10 == 8 && (n / 10) % 10 == 8) return 2 + count8(n/10);
+  else if (n % 10 == 8) return 1 + count8(n/10);
+  else return count8(n/10);
+}
